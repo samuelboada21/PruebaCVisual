246db0c3a9b873bac4428229a67ba3b858499617
samuelboada21 <samuelomarboba@ufps.edu.co>
2025-01-31 13:41:35 -0500

    Conexion a la db sql server

--- PruebaCVisual/Program.cs
+++ PruebaCVisual/Program.cs
@@ -1,23 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Data.SqlClient;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+// Construir la aplicacion
 var app = builder.Build();
+
+//Traigo la cadena de conexion desde el archivo appsetting
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Iniciar conexion a la db
+try
+{
+    using (var connection = new SqlConnection(connectionString))
+    { 
+        await connection.OpenAsync();
+        Console.WriteLine("Conexion a la base de datos exitosa");
+    }
+}
+catch (Exception ex)
+{ 
+    Console.WriteLine($"Error al conectar con la base de datos: {ex.Message}");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
